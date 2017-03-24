17002b3cef7e14f0d29858e787b1d84395184f68
Rasmus Thrane <snubi68@gmail.com>
2017-03-24 09:09:07 +0100

    b

---
+++ src/Model/Model.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by rasmusthrane on 24/03/17.
+ */
+public class Model {
+}
---
+++ src/View/View.java
@@ -0,0 +1,7 @@
+package View;
+
+/**
+ * Created by rasmusthrane on 24/03/17.
+ */
+public class View {
+}
